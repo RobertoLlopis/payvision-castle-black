856dff978a517aa8884e9d750bd5419873ac954e
Roberto Llopis <roberto.llopis.developer@gmail.com>
2021-02-21 18:27:37 +0100

    Basic authoritation

--- 1.CastleBlack/src/api.js
+++ 1.CastleBlack/src/api.js
@@ -17,6 +17,26 @@ const {
 // This was my data source. I move it to separate entities controllers  ////will be your data source
 
 //===================================
+//===Basic authentication middleware
+//===================================
+api.use((req, res, next) => {
+  const auth = { user: "rolmaster", password: "whowillwin?" };
+  if (req.headers.authorization) {
+    const b64auth = req.headers.authorization.split(" ")[1];
+    const [user, password] = Buffer.from(b64auth, "base64")
+      .toString()
+      .split(":");
+    if (user && password && user === auth.user && password === auth.password) {
+      // Access granted
+      return next();
+    }
+    res.status(401).send("Wrong credentials.");
+  }
+  // Access denied
+  res.status(401).send("Basic Authentication header required.");
+});
+
+//===================================
 //====== Objects entity endpoints
 //===================================
 // EXAMPLE ENDPOINT: LIST ALL OBJECTS
