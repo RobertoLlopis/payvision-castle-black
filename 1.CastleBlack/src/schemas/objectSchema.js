cb4e1639633bb8a110824ea84ead3210e973be41
Roberto Llopis <roberto.llopis.developer@gmail.com>
2021-02-21 17:42:50 +0100

    Improve Object schema

--- 1.CastleBlack/src/schemas/objectSchema.js
+++ 1.CastleBlack/src/schemas/objectSchema.js
@@ -2,8 +2,8 @@ const schema = require("schm");
 
 const objectSchema = schema({
   id: Number,
-  name: String,
-  value: Number,
+  name: { type: String, required: true },
+  value: { type: Number, required: true },
 });
 
 module.exports = {
